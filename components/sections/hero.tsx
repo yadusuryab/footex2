5d039e8a93d3543e8871fa5ee53fd4430789a2e2
Yadu <yadusurya666@gmail.com>
2026-07-16 22:17:46 +0530

    c

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Poster2 from "@/public/p3.jpg";
+import Poster2 from "@/public/p3.jpeg";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import Image from "next/image";
